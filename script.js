0d2271865aedde4e7e8934519b23774419469b3d
Rastar Music <rastarmusic@MacBook-Pro.local>
2021-09-15 16:37:29 -0400

    randomization

--- script.js
+++ script.js
@@ -10,74 +10,61 @@ function writePassword() {
   passwordText.value = password;
 
 }
-function generatePassword() {
-  numberofchar = prompt("How Many Characters do you want?");
-  if(numberofchar <= 8 || numberofchar >= 128){
-  alert("Must be between 8 and 128 characters")
-  } else{
-  upperCase = confirm("Confirm Use of uppercase letters");
-  if(upperCase === true){
-    console.log("UseUppercase");
-  } 
-
-  lowerCase = confirm("Use of lowercase letters?");
-  if(lowerCase === true){
-    console.log("uselowercase");
-  }
-  specialchar = confirm("Confirm Use of special characters?");
-  if(specialchar === true){
-    console.log("usespecialcharacters")
-  }
-  numeric = confirm ("Confirm use of numbers");
-  if(numeric === true){
-    console.log("includenumbers");
-  }
-
-  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-  var specialchar = ["!","@","#","$","%","&","=","+","-"]
-  var numeric = ["1","2","3","4","5","6","7","8","9"]  
-
-//console.log(upperCase.length)
-
-
-//upperCase = upperCase.split("");
-//lowerCase = lowerCase.split("");
-//specialchar = specialchar.split("");
-//numeric = numeric.split("");
-
-
-//var myArray = [upperCase,lowerCase,specialchar,numeric];
-
-// declare empty array
-//var randomVal = [];
-//var randomVal = upperCase[Math.floor(Math.random() * upperCase.length)]; 
-//console.log(upperCase.length)
-
-
-// loop through myArray and assign random values to myNewArray
-//for(var i = 0; i < 27; i++){
-
-  // get random number
-  //var randomVal = myArray[Math.floor(Math.random() * myArray.length)];  
-
-  // update new array
-  //myNewArray.push(randomVal);
-
-  
-
-// make sure it works!
-//console.log(myNewArray);
-
-
-  }
-  
-  
-return '' 
-
-}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUV";
+  var lowerCase = "abcdefghijklmnopqrstuv";
+  var specialchar = "!@#$%^&*=-+";
+  var numbers = "123456789";
+  var bulkPass = "";
+  var finalPass = "";
+  
+  var passLength = prompt("Password should be between 8 to 128 characters! How long you want your password to be?");
+  
+  var lowerletters = confirm("Confirm use of lowercase letters?");
+  if (lowerletters){
+     bulkPass += lowerCase;
+     
+  };
+  var upperletters = confirm("Confirm use of uppercase letters?");
+  if (upperletters){
+    bulkPass += upperCase;
+  };
+  var numBers = confirm("Confirm the use of Numbers?");  
+  if (numBers) {
+    bulkPass += numbers;
+    
+  };
+  var specialChar = confirm("Confirm the use of special characters?")
+  if (specialChar) {
+    bulkPass += specialchar;
+    console.log(bulkPass)
+  };
+  
+  if(passLength >= 8 && passLength <= 128){
+    for (let i = 0; i < passLength; i++) {
+      finalPass += bulkPass.charAt(
+        Math.floor(Math.random() * bulkPass.length));
+    };
+    
+  }else {
+    alert("Password must be between 8 and 128 characters");
+  };
+  confirm("Confirm to see new password");
+  return finalPass;
+  
+};
+
+
+  
+ 
+  
+   
+
+
 
 
 
